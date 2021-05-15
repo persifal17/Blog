40f3248cbc38d2645866fc0acbe2ea83b95be819
persifal17 <73614450+persifal17@users.noreply.github.com>
2021-05-15 21:32:05 +0300

    ,

---
+++ app/Http/Controllers/Blog/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    /**
+     * BaseController constructor
+     */
+    public function __construct()
+    {
+        //Ініціалізація загальних елементів адмінки
+    }
+
+}
